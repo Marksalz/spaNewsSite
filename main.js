ecd36b3391ccd72a6580ad33168581506c8c1f9c
Mark <menachemsalzberg13@gmail.com>
2025-08-25 09:13:26 +0300

    Implement news fetching and display functionality with updated styles

--- main.js
+++ main.js
@@ -5,13 +5,48 @@ const pages = ["news", "create story"];
 root.appendChild(GenNavbar(pages));
 root.appendChild(main);
 
-function loadPage(pageName = "news") {
+async function loadNews() {
+  const localStorage = window.localStorage;
+  let news = null;
+  console.log(`local storage: `, localStorage);
+  if (localStorage.length === 0) {
+    try {
+      const response = await fetch(
+        `https://newsapi.org/v2/everything?q=trump`,
+        {
+          method: "GET",
+          headers: {
+            "X-Api-Key": config.API_KEY,
+          },
+        }
+      );
+      const newsFromApi = await response.json();
+      news = newsFromApi;
+      console.log(`News:`, newsFromApi);
+      localStorage.setItem("news", JSON.stringify(newsFromApi));
+    } catch (error) {
+      console.error("Failed to fetch news:", error);
+    }
+  } else {
+    const newsData = localStorage.getItem("news");
+    if (newsData) {
+      news = JSON.parse(newsData);
+      console.log("Loaded news from localStorage:", news);
+    } else {
+      console.log("No news found in localStorage.");
+    }
+  }
+  return news;
+}
+
+async function loadPage(pageName = "news") {
+  const news = await loadNews();
   const main = document.querySelector("main");
   main.innerHTML = "";
   let page;
   switch (pageName) {
     case "news":
-      page = News();
+      page = loadNewsToWindow(news);
       main.appendChild(page);
       break;
     case "create story":
@@ -19,8 +54,8 @@ function loadPage(pageName = "news") {
       main.appendChild(page);
       break;
     default:
-      page = News();
-      main.appendChild(page);
+      //   page = News();
+      //   main.appendChild(page);
       break;
   }
 }
